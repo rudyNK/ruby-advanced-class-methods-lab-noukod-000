09d3c34f831b17e8b8072241ff104e5a3f8fca83
Rudy Ravelin <rudy@noukod.com>
2018-10-02 20:16:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,63 +1,56 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
-  def self.all
+   def self.all
     @@all
   end
-
-  def save
+   def save
     self.class.all << self
   end
-
-  def self.create
-    song = self.new
-    #@@all << song
+  # Given
+   def self.create
+    song = Song.new
+    song.save
     song
   end
-
-  def self.new_by_name(name)
+   def self.new_by_name(song_name)
     song = self.new
-    song.name = name
+    song.name = song_name
     song
   end
-
-  def self.create_by_name(name)
-    song = self.new
-    song.name = name
-    @@all << song
+   def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
     song
   end
-
-  def self.find_by_name(name)
-    @@all.find{|x| x.name == name}
+   def self.find_by_name(song_name)
+    self.all.detect{|s| s.name == song_name}
   end
-
-    def self.alphabetical()
-    #returns all the songs instances in ascending (a-z) alphabetical order.
-    @@all.sort_by{|x| x.name}
+   def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
   end
-
-
-  def self.new_from_filename(name)
-    song = self.new
-    song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split(" - ")[0])
+   def self.alphabetical
+    self.all.sort_by{|s| s.name}
+  end
+   def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+     song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
     song
   end
-
-  def self.create_from_filename(name)
-  #class method should not only parse the filename correctly but should also save the song
-    song = self.new
-    song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split(" - ")[0])
-    @@all << song
+   def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+     song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
     song
   end
-
-  def self.destroy_all()
-    #reset the state of the @@all class variable to an empty array thereby deleting all previous song instances.
-    @@all.clear
+   def self.destroy_all
+    self.all.clear
   end
-
-end
+ end
